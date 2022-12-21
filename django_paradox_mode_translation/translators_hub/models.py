f74baa2897b7e8edb07066427d5846d19f908ded
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-12-21 12:10:38 +0300

    Написана модель для хранения данных каждой страницы.

--- django_paradox_mode_translation/translators_hub/models.py
+++ django_paradox_mode_translation/translators_hub/models.py
@@ -1,3 +1,41 @@
+import django.contrib.auth.models
 from django.db import models
 
-# Create your models here.
+
+class ModTranslation(models.Model):
+    title = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название',
+    )
+
+    mode_name = models.CharField(
+        max_length=100,
+        blank=False,
+        unique=True,
+        verbose_name='Перевод для модификации:'
+    )
+
+    steam_link = models.URLField(
+        max_length=200,
+        blank=True,
+        verbose_name='Ссылка на мастерскую'
+    )
+
+    paradox_plaza_link = models.URLField(
+        max_length=200,
+        blank=True,
+        verbose_name='Ссылка на Плазу'
+    )
+
+    image = models.ImageField(
+        verbose_name='Иллюстрация'
+    )
+
+    authors = models.ManyToManyField(
+        to=django.contrib.auth.models.User
+    )
+
+    views = models.PositiveIntegerField(
+        default=0
+    )
