d1978f5c8e493ab1aea98899f86a14a883d173ac
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-12-23 14:46:10 +0300

    Зарегистрированы новые модели в админке

--- django_paradox_mode_translation/translators_hub/admin.py
+++ django_paradox_mode_translation/translators_hub/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from translators_hub.models import ModTranslation, UserProfile
+
+admin.site.register(ModTranslation)
+admin.site.register(UserProfile)
