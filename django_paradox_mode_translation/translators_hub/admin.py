15c1e0c0d569d008eda71b8429d14619fc1276cf
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-02-15 12:44:56 +0300

    Update admin.py

--- django_paradox_mode_translation/translators_hub/admin.py
+++ django_paradox_mode_translation/translators_hub/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-from translators_hub.models import ModTranslation, UserProfile
+from translators_hub.models import ModTranslation, UserProfile, Titles, ProfileComments, Game, Roles, Invites
 
 admin.site.register(ModTranslation)
+admin.site.register(Roles)
+admin.site.register(Invites)
 admin.site.register(UserProfile)
+admin.site.register(Titles)
+admin.site.register(ProfileComments)
+admin.site.register(Game)
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
