43d9baad0acb6278c3c2a8fee28109553ef891c4
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-02-22 14:45:09 +0300

    Update views.py

--- django_paradox_mode_translation/translators_hub/views.py
+++ django_paradox_mode_translation/translators_hub/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import views, login, authenticate, get_user_model
 from django.contrib.auth.models import AnonymousUser
+from django.contrib import messages
 from django.db.models import Q
-from django.forms import model_to_dict
+from django.forms import model_to_dict, modelformset_factory
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.utils.text import slugify
 from django.views import generic
 
 import translators_hub.models
-from .forms import UpdateProfileForm, RegistrationForm, AddPageForm, ServiceForm, ProfileFormForApply, InviteUserForm
+from . import models
+from .forms import UpdateProfileForm, RegistrationForm, AddPageForm, ServiceForm, ProfileFormForApply, InviteUserForm, \
+    UpdateUserForm, ChangeUserRoleForm
 from .models import ModTranslation, UserProfile, ProfileComments, Roles, Invites
 
 User = get_user_model()
@@ -18,9 +21,13 @@ User: translators_hub.models.User
 class HomeView(generic.ListView):
     template_name = 'translators_hub/home.html'
     context_object_name = 'latest_pages_list'
+    paginate_by = 4
+
+    def __init__(self, *args, **kwargs):
+        super(HomeView, self).__init__(*args, **kwargs)
 
     def get_queryset(self):
-        return ModTranslation.objects.order_by('-created_date')[:10]
+        return ModTranslation.objects.order_by('-created_date')
 
 
 class DetailView(generic.DetailView):
@@ -51,9 +58,72 @@ class DetailView(generic.DetailView):
             return redirect(reverse_lazy('translators_hub:apply_for', kwargs={'slug': slug}))
 
     def get(self, request, *args, **kwargs):
-        if self.get_object().authors.get(user=request.user).role in [Roles.ORGANISER, Roles.MODERATOR]:
-            self.extra_context = {'moderator': True}
-        return super(DetailView, self).get(request, args, kwargs)
+        try:
+            if not isinstance(request.user, AnonymousUser):
+                role_object = self.get_object().authors.get(user=request.user)
+                if role_object.role in [Roles.ORGANISER, Roles.MODERATOR]:
+                    self.extra_context = {'moderator': True}
+            return super(DetailView, self).get(request, args, kwargs)
+        except models.models.ObjectDoesNotExist:
+            return super(DetailView, self).get(request, args, kwargs)
+
+
+class ManagementView(generic.View):
+    template_name = 'translators_hub/project_management.html'
+
+    def get(self, request, slug, *args, **kwargs):
+        project = ModTranslation.objects.get(slug=slug)
+        project_authors = None
+        project_moderator = None
+        if not isinstance(request.user, AnonymousUser):
+            project_authors = project.authors.all().prefetch_related('user', 'user__userprofile')
+            project_moderator = project.authors.filter(role__in=[Roles.ORGANISER, Roles.MODERATOR],
+                                                       user=request.user)
+        if project_moderator:
+            context = {
+                'slug': slug,
+                'user': request.user,
+                'authors': project_authors,
+            }
+            return render(request=request, template_name='translators_hub/project_management.html', context=context)
+        else:
+            error_message = "У вас недостаточно прав для захода на эту страницу"
+            messages.add_message(request=request, level=messages.ERROR, message=error_message)
+            context = {
+                'current_page': project,
+            }
+            return render(request=request, template_name='translators_hub/detail_page.html', context=context)
+
+    def post(self, request, slug, *args, **kwargs):
+        project = ModTranslation.objects.get(slug=slug)
+        if request.POST.get('fired'):
+            fired_users = Roles.objects.filter(user_id__in=request.POST.get('fired'))
+            for author in fired_users:
+                project.authors.remove(author)
+                project.save()
+            return redirect('translators_hub:management', slug=slug)
+        else:
+            return redirect('translators_hub:management', slug=slug)
+
+
+class ChangeRoleView(generic.edit.FormView):
+    form_class = ChangeUserRoleForm
+    template_name = 'translators_hub/change_user_role.html'
+    success_url = ''
+
+    def get(self, request, *args, **kwargs):
+        userprofile = User.objects.get(username=kwargs.get('username')).userprofile
+        self.extra_context = {'changing_user': kwargs.get('username'), 'userprofile': userprofile}
+        return super(ChangeRoleView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        user = User.objects.get(username=kwargs.get('username'))
+        mod_translation = ModTranslation.objects.get(slug=kwargs.get('slug'))
+        old_role = user.roles.get(modtranslation=mod_translation)
+        new_role = Roles.objects.get_or_create(user=user, role=request.POST.get('role'))[0]
+        mod_translation.authors.remove(old_role)
+        mod_translation.authors.add(new_role)
+        return redirect('translators_hub:management', slug=kwargs.get('slug'))
 
 
 class ProfileView(generic.edit.FormMixin, generic.DetailView):
@@ -65,8 +135,15 @@ class ProfileView(generic.edit.FormMixin, generic.DetailView):
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
+        if isinstance(request.user, AnonymousUser):
+            error_message = "Для просмотра профилей наших пользователей - авторизуйтесь!"
+            messages.add_message(request=request, level=messages.ERROR, message=error_message)
+            return redirect('translators_hub:home')
         if request.user.userprofile == self.object:
             self.initial = model_to_dict(self.object)
+            user_form = UpdateUserForm(data=request.POST if request.POST != {} else None,
+                                       initial=model_to_dict(request.user))
+            self.extra_context = {'user_form': user_form}
             context = self.get_context_data(object=self.object)
             return self.render_to_response(context)
         else:
@@ -79,18 +156,23 @@ class ProfileView(generic.edit.FormMixin, generic.DetailView):
         if request.user.is_authenticated:
             user_profile = request.user.userprofile
             self.success_url = reverse_lazy('translators_hub:profile', kwargs={'slug': kwargs['slug']})
+            self.kwargs['instance'] = user_profile
             form = self.get_form()
-            print(model_to_dict(user_profile))
-            if form.is_valid():
-                form_data = form.save(commit=False)
-                form_data.pk = user_profile.pk
-                form_data.user = request.user
-                form_data.slug = kwargs['slug']
-                form_data.image = user_profile.profile_image
-                form_data.reputation = user_profile.reputation
-                form_data.save()
-                form.save_m2m()
+            user_form = UpdateUserForm(request.POST, instance=request.user)
+
+            if form.is_valid() and user_form.is_valid():
+                form.save(commit=True)
+                user_form.save()
                 return redirect(self.success_url)
+            else:
+                return self.get(request=request, form_errors=user_form.errors, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(ProfileView, self).get_form_kwargs()
+        instance = self.kwargs.get('instance')
+        if instance:
+            kwargs.update({'instance': instance})
+        return kwargs
 
 
 class LogInView(views.LoginView):
@@ -122,6 +204,14 @@ class AddPageView(generic.FormView):
     template_name = 'translators_hub/add_page.html'
     form_class = AddPageForm
     success_url = None
+
+    def get(self, request, *args, **kwargs):
+        if isinstance(request.user, AnonymousUser):
+            error_message = "Зарегистрируйтесь или войдите для этого действия"
+            messages.add_message(request=request, level=messages.ERROR, message=error_message)
+            return redirect('translators_hub:home')
+        else:
+            return super(AddPageView, self).get(args, kwargs)
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
@@ -187,9 +277,8 @@ class InvitesView(generic.ListView):
                 invite.status = Invites.DECLINED
                 invite.save()
         else:
-            self.extra_context = {
-                'errors': ['Ошибка чтения значения кнопки, повторите попытку']
-            }
+            error_message = "Ошибка чтения значения кнопки, повторите попытку"
+            messages.add_message(request=request, level=messages.ERROR, message=error_message)
         return redirect(reverse('translators_hub:invites', kwargs={'slug': slug}))
 
     def get(self, request, *args, **kwargs):
@@ -211,17 +300,21 @@ class SendInvitesView(generic.ListView):
         self.moderators = []
         self.invited_authors = None
         self.current_data = None
+        self.current_authors = None
 
     def get_queryset(self):
         slug = self.request.resolver_match.kwargs['slug']
         self.current_data = ModTranslation.objects.get(slug=slug)
-        for author in self.current_data.authors.all():
+        self.current_authors = self.current_data.authors.all().prefetch_related('user')
+        for author in self.current_authors:
             if author.role in [Roles.ORGANISER, Roles.MODERATOR]:
                 self.moderators.append(author.user)
             else:
                 self.active_authors.append(author.user)
         self.invited_authors = [invite.target for invite in Invites.objects.filter(mod_translation=self.current_data)]
-        free_users = User.objects.exclude(username__in=self.active_authors + self.invited_authors + self.moderators)
+        free_users = User.objects.exclude(
+            username__in=self.active_authors + self.invited_authors + self.moderators).prefetch_related('userprofile',
+                                                                                                        'userprofile__titles')
         return free_users
 
     def get(self, request, *args, **kwargs):
@@ -229,12 +322,12 @@ class SendInvitesView(generic.ListView):
         self.extra_context = {'form': form, 'slug': kwargs.get('slug')}
         super_method = super(SendInvitesView, self).get(request, args, kwargs)
         moderators = [author.user if author.role in [Roles.ORGANISER, Roles.MODERATOR] else '' for author in
-                      self.current_data.authors.all()]
-        print(moderators)
+                      self.current_authors]
         if request.user not in moderators:
+            error_message = "У вас нет прав на приглашение новых людей в этот проект"
+            messages.add_message(request=request, level=messages.ERROR, message=error_message)
             context = {
                 'current_page': self.current_data,
-                'errors': ["У вас нет прав на приглашение новых людей в этот проект", ],
             }
             return render(request=request, template_name='translators_hub/detail_page.html', context=context)
         else:
