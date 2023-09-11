33eea6978a72ea7dcd7d239924e4ca9c63ddf7f7
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-09-11 14:30:46 +0300

    Update urls.py

--- django_paradox_mode_translation/django_paradox_mode_translation/urls.py
+++ django_paradox_mode_translation/django_paradox_mode_translation/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,5 +27,5 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     import debug_toolbar
-    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
 
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
--- django_paradox_mode_translation/translators_hub/urls.py
+++ django_paradox_mode_translation/translators_hub/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
+from django_paradox_mode_translation import settings
 from . import views
+from django.conf.urls.static import static
 
 app_name = 'translators_hub'
 urlpatterns = [
