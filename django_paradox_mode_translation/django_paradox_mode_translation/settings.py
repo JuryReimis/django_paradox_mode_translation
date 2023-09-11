13e046d134baf576d6c1fd8bb16716688dff04a3
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-09-11 14:29:32 +0300

    Update settings.py

--- django_paradox_mode_translation/django_paradox_mode_translation/settings.py
+++ django_paradox_mode_translation/django_paradox_mode_translation/settings.py
@@ -134,6 +134,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = []
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
