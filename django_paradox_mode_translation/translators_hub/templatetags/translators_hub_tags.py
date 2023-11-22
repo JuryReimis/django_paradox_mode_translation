9aec662234a211dca9592e2c09fefcd54e50ecc8
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-11-22 13:30:57 +0300

    Создано несколько тегов. Создан тэг для заполнения блока пагинации. Создан тег для получения комментариев со страницы.

--- django_paradox_mode_translation/translators_hub/templatetags/translators_hub_tags.py
+++ django_paradox_mode_translation/translators_hub/templatetags/translators_hub_tags.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import AnonymousUser
 from django.template import Library
 
-from translators_hub.models import Invites, Roles, ModTranslation, User
+from translators_hub.models import Invites, ProfileComments, UserProfile
+from translators_hub.utils.custom_paginator import CustomPaginator
 
 register = Library()
 
@@ -21,7 +22,30 @@ def get_nav_bar(context):
                 }
 
 
+@register.inclusion_tag(filename='translators_hub/pagination.html')
+def get_pagination_block(page_obj):
+    context = {
+        'page_obj': page_obj,
+    }
+    return context
+
+
 @register.simple_tag()
 def get_role_display(role):
     display = Invites(role=role).get_role_display()
     return display
+
+
+@register.simple_tag(takes_context=True)
+def get_comments(context, target):
+    object_list = []
+    if isinstance(target, UserProfile):
+        object_list = ProfileComments.objects.filter(target=target).order_by('pub_date')
+
+    if object_list:
+        paginator = CustomPaginator(object_list, per_page=5)
+    else:
+        paginator = None
+    current_page = context.request.GET.get('page', 1)
+    page_obj = paginator.get_page(current_page) if paginator else None
+    return page_obj
