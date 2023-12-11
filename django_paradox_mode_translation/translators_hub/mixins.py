792132cf30276f68c8abd04f01b120f30849a709
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-12-11 17:51:38 +0300

    AddCommentMixin расширен для использования на страницах проектов.

--- django_paradox_mode_translation/translators_hub/mixins.py
+++ django_paradox_mode_translation/translators_hub/mixins.py
@@ -2,8 +2,8 @@ from django.contrib import messages
 from django.shortcuts import redirect, get_object_or_404
 from django.views import generic
 
-from translators_hub.forms import AddProfileCommentForm
-from translators_hub.models import UserProfile, ProfileComments
+from translators_hub.forms import AddProfileCommentForm, AddProjectCommentForm
+from translators_hub.models import UserProfile, ProfileComments, ModTranslation, ProjectComments
 
 
 class AddCommentMixin(generic.edit.ModelFormMixin):
@@ -15,6 +15,10 @@ class AddCommentMixin(generic.edit.ModelFormMixin):
         current_object = self.get_object()
         if isinstance(current_object, UserProfile):
             self.form_class = AddProfileCommentForm
+            self.target = current_object
+            self.success_url = current_object.get_absolute_url()
+        elif isinstance(current_object, ModTranslation):
+            self.form_class = AddProjectCommentForm
             self.target = current_object
             self.success_url = current_object.get_absolute_url()
 
@@ -31,14 +35,17 @@ class AddCommentMixin(generic.edit.ModelFormMixin):
             if isinstance(current_object, UserProfile):
                 comment = get_object_or_404(ProfileComments, pk=delete_id)
                 comment.visible = False
-                try:
-                    comment.save()
-                except Exception as error:
-                    error_text = f'Ошибка удаления\n{error}'
-                    messages.add_message(request, level=messages.ERROR, message=error_text)
-                else:
-                    message_text = "Комментарий удален!"
-                    messages.add_message(request, level=messages.SUCCESS, message=message_text)
+            elif isinstance(current_object, ModTranslation):
+                comment = get_object_or_404(ProjectComments, pk=delete_id)
+                comment.visible = False
+            try:
+                comment.save()
+            except Exception as error:
+                error_text = f'Ошибка удаления\n{error}'
+                messages.add_message(request, level=messages.ERROR, message=error_text)
+            else:
+                message_text = "Комментарий удален!"
+                messages.add_message(request, level=messages.SUCCESS, message=message_text)
         else:
             form: AddProfileCommentForm = self.get_form()
             form.instance.target = self.target
