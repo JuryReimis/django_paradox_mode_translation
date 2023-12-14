535113287aedd0c419df44e3984a2a1c256ce745
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-12-14 16:37:19 +0300

    Обновлен тег get_comments

--- django_paradox_mode_translation/translators_hub/templatetags/translators_hub_tags.py
+++ django_paradox_mode_translation/translators_hub/templatetags/translators_hub_tags.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AnonymousUser
+from django.db.models import Count, Case, When, Value, IntegerField
 from django.template import Library
 
 from translators_hub.models import Invites, ProfileComments, UserProfile, ModTranslation, ProjectComments
@@ -11,11 +12,13 @@ register = Library()
 def get_nav_bar(context):
     user = context['user']
     if isinstance(user, AnonymousUser):
-        return {'button_name': "Авторизоваться", 'login_status': "Войти", 'registration_status': "Регистрация", 'user': user}
+        return {'button_name': "Авторизоваться", 'login_status': "Войти", 'registration_status': "Регистрация",
+                'user': user}
     else:
         active_invites = user.target_name.filter(status=None)
         active_projects = user.roles.all()
-        return {'button_name': f'{user}', 'login_status': f'{user}', 'registration_status': "Выйти", 'user_logged': user.userprofile,
+        return {'button_name': f'{user}', 'login_status': f'{user}', 'registration_status': "Выйти",
+                'user_logged': user.userprofile,
                 'user': user,
                 'active_invites': active_invites,
                 'active_projects': active_projects
@@ -40,9 +43,19 @@ def get_role_display(role):
 def get_comments(context, target):
     object_list = []
     if isinstance(target, UserProfile):
-        object_list = ProfileComments.objects.filter(target=target, visible=True).order_by('-pub_date')
+        object_list = ProfileComments.objects.filter(target=target, visible=True).select_related('author').annotate(
+            likes=Count(
+                Case(When(profile_comment_reactions__reaction=True, then=Value(1)), output_field=IntegerField())),
+            dislikes=Count(
+                Case(When(profile_comment_reactions__reaction=False, then=Value(1)), output_field=IntegerField())),
+        ).order_by('-pub_date')
     elif isinstance(target, ModTranslation):
-        object_list = ProjectComments.objects.filter(target=target, visible=True).order_by('-pub_date')
+        object_list = ProjectComments.objects.filter(target=target, visible=True).select_related('author').annotate(
+            likes=Count(
+                Case(When(project_comment_reactions__reaction=True, then=Value(1)), output_field=IntegerField())),
+            dislikes=Count(
+                Case(When(project_comment_reactions__reaction=False, then=Value(1)), output_field=IntegerField())),
+        ).order_by('-pub_date')
     if object_list:
         paginator = CustomPaginator(object_list, per_page=5)
     else:
