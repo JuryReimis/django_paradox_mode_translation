5c10618952919d449e22664f962488fe9e0ce983
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-19 17:24:51 +0300

    Update urls.py

--- django_paradox_mode_translation/teams/urls.py
+++ django_paradox_mode_translation/teams/urls.py
@@ -7,5 +7,6 @@ app_name = 'teams'
 urlpatterns = [
     path('', views.TeamsView.as_view(), name='home'),
     path('<slug:slug>/', views.TeamPageView.as_view(), name='team_detail'),
-    path('<slug:slug>/invite_users/', views.SendInviteView.as_view(), name='invite_user')
+    path('<slug:slug>/invite_users/', views.SendInviteView.as_view(), name='invite_user'),
+    path('invites', views.TeamInvitesHandler.as_view(), name='invites')
 ]
