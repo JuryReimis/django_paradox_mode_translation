7937fcda68a9177b2520deae88210cfcff5c04f4
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-26 18:58:44 +0300

    Расширена модель для запросов. Написана модель для темы запроса Изменено поле статуса заявки и добавлено поле причины отказа.

--- django_paradox_mode_translation/moderators/models.py
+++ django_paradox_mode_translation/moderators/models.py
@@ -3,6 +3,18 @@ from django.db import models
 
 class Query(models.Model):
 
+    NO_MODERATOR = None
+    IN_WORK = 'w'
+    COMPLETE = 'c'
+    DENIED = 'd'
+
+    STATUS = [
+        (NO_MODERATOR, "Нет исполнителя"),
+        (IN_WORK, "В работе"),
+        (COMPLETE, "Выполнено"),
+        (DENIED, "В запросе отказано")
+    ]
+
     query_text = models.CharField(
         max_length=4500,
         blank=False,
@@ -31,11 +43,12 @@ class Query(models.Model):
         auto_now_add=True
     )
 
-    status = models.ForeignKey(
-        to='Status',
-        on_delete=models.PROTECT,
-        related_name='status_queries',
-        verbose_name="Статус заявки",
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS,
+        default=None,
+        null=True,
+        verbose_name="Статус заявки"
     )
 
     query_considered = models.ForeignKey(
@@ -45,6 +58,14 @@ class Query(models.Model):
         null=True,
         default=None,
         verbose_name="Модератор, принявший заявку"
+    )
+
+    denial_reason = models.CharField(
+        max_length=1000,
+        blank=False,
+        default=None,
+        null=True,
+        verbose_name="Причина отказа"
     )
 
     accept_date = models.DateTimeField(
@@ -65,9 +86,11 @@ class Query(models.Model):
 
 class Topic(models.Model):
 
-    pass
+    topic_title = models.CharField(
+        max_length=120,
+        blank=False,
+        verbose_name="Тема обращения"
+    )
 
-
-class Status(models.Model):
-
-    pass
+    def __str__(self):
+        return f'Тема - {self.topic_title}'
