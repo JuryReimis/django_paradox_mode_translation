8a0e7d4581c554fe3d001e77c8415988a65e3117
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-26 18:27:22 +0300

    Прописан маршрут к домашней странице раздела для взаимодействия с модераторами

--- django_paradox_mode_translation/moderators/urls.py
+++ django_paradox_mode_translation/moderators/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
+from moderators.views import ModeratorHomeView
 
+app_name = 'moderators'
+
+urlpatterns = [
+    path('', ModeratorHomeView.as_view(), name='home'),
+]
