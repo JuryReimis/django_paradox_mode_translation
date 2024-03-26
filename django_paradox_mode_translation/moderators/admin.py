6dd4f95a9a90b07c8b8ea660b9b5022e9f8ff9fc
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-26 19:00:12 +0300

    Зарегистрирована модель темы обращения.

--- django_paradox_mode_translation/moderators/admin.py
+++ django_paradox_mode_translation/moderators/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from moderators.models import Query
+from moderators.models import Query, Topic
 
 admin.site.register(Query)
-
+admin.site.register(Topic)
