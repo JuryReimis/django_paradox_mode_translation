f88b0adcb8a7af2a846af3b26cb73fa146ad6074
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-28 14:08:00 +0300

    Созданы классы для отображения домашней страницы и страницы заполнения формы

--- django_paradox_mode_translation/moderators/views.py
+++ django_paradox_mode_translation/moderators/views.py
@@ -1,1 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+
+from django.views import View
+from django.views.generic import FormView
+
+from moderators.forms import SendQueryForm
+
+
+class ModeratorHomeView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name='moderators/home.html')
+
+
+class SendQueryView(FormView):
+
+    template_name = 'moderators/send_query.html'
+    form_class = SendQueryForm
+    success_url = reverse_lazy('moderators:home')
+
+    def get(self, request, *args, **kwargs):
+        return super(SendQueryView, self).get(request, *args, **kwargs)
+
+
