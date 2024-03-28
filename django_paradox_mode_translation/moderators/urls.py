495dc3a02de05d04ec1c8a0755b850fa60c70f99
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-28 15:37:12 +0300

    Добавлен маршрут для страницы отображения отправленных заявок

--- django_paradox_mode_translation/moderators/urls.py
+++ django_paradox_mode_translation/moderators/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from moderators.views import ModeratorHomeView, SendQueryView
+from moderators.views import ModeratorHomeView, SendQueryView, MyQueriesView
 
 app_name = 'moderators'
 
 urlpatterns = [
     path('', ModeratorHomeView.as_view(), name='home'),
     path('send-query/', SendQueryView.as_view(), name='send_query'),
+    path('my-queries/', MyQueriesView.as_view(), name='my_queries'),
 ]
