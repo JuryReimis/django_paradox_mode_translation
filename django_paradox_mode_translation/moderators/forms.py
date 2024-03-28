c5984198bcab75779f49d8862f9642e26839345a
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-28 14:03:46 +0300

    Создана форма для отправки заявки модераторам

---
+++ django_paradox_mode_translation/moderators/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from moderators.models import Query, Topic
+
+
+class SendQueryForm(forms.ModelForm):
+
+    query_text = forms.CharField(
+        widget=forms.Textarea(attrs={
+
+        }),
+        required=True,
+        label="Текст заявки"
+    )
+
+    topic_of_query = forms.ModelChoiceField(
+        widget=forms.Select(attrs={
+
+        }),
+        queryset=Topic.objects.filter(),
+        label="Тема заявки"
+    )
+
+    class Meta:
+        model = Query
+        fields = ['query_text', 'topic_of_query']
