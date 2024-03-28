e3fc94e7a33cb6f222e67a068647295e4cd20607
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-28 14:39:52 +0300

    Добавлена обработка отправленной формы и сохранение новой заявки в бд.

--- django_paradox_mode_translation/moderators/views.py
+++ django_paradox_mode_translation/moderators/views.py
@@ -22,4 +22,12 @@ class SendQueryView(FormView):
     def get(self, request, *args, **kwargs):
         return super(SendQueryView, self).get(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return super(SendQueryView, self).post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.query_author = self.request.user
+        form.instance.status = None
+        form.save()
+        return super(SendQueryView, self).form_valid(form)
 
