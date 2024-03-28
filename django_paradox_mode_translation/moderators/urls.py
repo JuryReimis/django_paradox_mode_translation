a80cce9e43d98288a9d37a4b0120a821781e2250
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-28 14:06:19 +0300

    Добавлен маршрут для перехода на страницу заполнения формы

--- django_paradox_mode_translation/moderators/urls.py
+++ django_paradox_mode_translation/moderators/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from moderators.views import ModeratorHomeView
+from moderators.views import ModeratorHomeView, SendQueryView
 
 app_name = 'moderators'
 
 urlpatterns = [
     path('', ModeratorHomeView.as_view(), name='home'),
+    path('send-query/', SendQueryView.as_view(), name='send_query'),
 ]
