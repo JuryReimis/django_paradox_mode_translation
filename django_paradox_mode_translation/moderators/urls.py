c55d37c1ff14aeed032513b2b21d100e5f79aa96
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-29 12:54:41 +0300

    Добавлен маршрут на страницу бронирования заявок

--- django_paradox_mode_translation/moderators/urls.py
+++ django_paradox_mode_translation/moderators/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from moderators.views import ModeratorHomeView, SendQueryView, MyQueriesView
+from moderators.views import ModeratorHomeView, SendQueryView, MyQueriesView, QueryBookingView
 
 app_name = 'moderators'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', ModeratorHomeView.as_view(), name='home'),
     path('send-query/', SendQueryView.as_view(), name='send_query'),
     path('my-queries/', MyQueriesView.as_view(), name='my_queries'),
+    path('query-booking', QueryBookingView.as_view(), name='query_booking')
 ]
