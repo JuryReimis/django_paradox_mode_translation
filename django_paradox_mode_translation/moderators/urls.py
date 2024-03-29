e255654d10cc7421cda5328304016ce88d9f7cf2
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-03-29 15:14:48 +0300

    Добавлен маршрут для отображения страницы с принятыми в работу заявками

--- django_paradox_mode_translation/moderators/urls.py
+++ django_paradox_mode_translation/moderators/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from moderators.views import ModeratorHomeView, SendQueryView, MyQueriesView, QueryBookingView
+from moderators.views import ModeratorHomeView, SendQueryView, MyQueriesView, QueryBookingView, QueriesInWorkView
 
 app_name = 'moderators'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', ModeratorHomeView.as_view(), name='home'),
     path('send-query/', SendQueryView.as_view(), name='send_query'),
     path('my-queries/', MyQueriesView.as_view(), name='my_queries'),
-    path('query-booking', QueryBookingView.as_view(), name='query_booking')
+    path('query-booking/', QueryBookingView.as_view(), name='query_booking'),
+    path('queries-in-work/', QueriesInWorkView.as_view(), name='queries_in_work')
 ]
