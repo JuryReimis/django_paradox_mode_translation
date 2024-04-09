d96c1a6fe2a299b140dc6a688da92da7024c3b36
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-10 00:39:03 +0300

    Добавлен маршрут на страницу поиска, исправлен вид маршрутов

--- django_paradox_mode_translation/teams/urls.py
+++ django_paradox_mode_translation/teams/urls.py
@@ -6,8 +6,9 @@ app_name = 'teams'
 
 urlpatterns = [
     path('', views.TeamsView.as_view(), name='home'),
-    path('create_team/', views.CreateTeamView.as_view(), name='create_team'),
+    path('create-team/', views.CreateTeamView.as_view(), name='create_team'),
     path('invites/', views.TeamInvitesHandler.as_view(), name='invites'),
+    path('search-team/', views.SearchTeamView.as_view(), name='search_team'),
     path('<slug:slug>/', views.TeamPageView.as_view(), name='team_detail'),
-    path('<slug:slug>/invite_users/', views.SendInviteView.as_view(), name='invite_user'),
+    path('<slug:slug>/invite-users/', views.SendInviteView.as_view(), name='invite_user'),
 ]
