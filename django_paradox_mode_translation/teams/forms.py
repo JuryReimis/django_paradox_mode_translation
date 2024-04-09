30aad324c128af0ae44c19deb91b530646f45bb5
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-09 19:32:36 +0300

    Определена форма для добавления новых команд.

--- django_paradox_mode_translation/teams/forms.py
+++ django_paradox_mode_translation/teams/forms.py
@@ -1,6 +1,37 @@
 from django import forms
 
-from teams.models import TeamInvites
+from teams.models import TeamInvites, Teams, TeamMembers
+
+
+class CreateTeamForm(forms.ModelForm):
+
+    team_title = forms.CharField(
+        widget=forms.TextInput(attrs={
+
+        }),
+        label="Название команды",
+    )
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+
+        }),
+        required=False,
+        label="Описание"
+    )
+
+    def save(self, user=None, commit=True):
+        instance = super(CreateTeamForm, self).save(commit)
+        if user:
+            team_member, created = TeamMembers.objects.get_or_create(team=instance, user=user)
+            if created:
+                team_member.role = TeamMembers.CREATOR
+                team_member.save()
+        return instance
+
+    class Meta:
+        model = Teams
+        fields = ['team_title', 'description']
 
 
 class InviteUserForm(forms.ModelForm):
