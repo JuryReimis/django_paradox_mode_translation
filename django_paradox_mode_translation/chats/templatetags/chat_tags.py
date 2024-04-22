bbbaeb0076ee702628cbdf678444030004c91843
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-22 18:26:36 +0300

    Наименование приведено в соответствие с новыми моделями

--- django_paradox_mode_translation/chats/templatetags/chat_tags.py
+++ django_paradox_mode_translation/chats/templatetags/chat_tags.py
@@ -1,6 +1,6 @@
 from django.template import Library
 
-from chats.forms import SendMessageForm
+from chats.forms import PrivateMessageForm
 
 register = Library()
 
@@ -8,11 +8,12 @@ register = Library()
 @register.inclusion_tag(takes_context=True, filename='chats/chat.html')
 def get_chats(context):
     user = context['user']
-    user_chats = user.chats.all().prefetch_related('messages', 'messages__author')
-    form = SendMessageForm()
+    private_chats = user.private_chats.all().prefetch_related('private_messages', 'private_messages__author')
+    private_form = PrivateMessageForm()
     context = {
         'user': user,
-        'user_chats': user_chats,
-        'form': form
+        'private_chats': private_chats,
+        'teams_chats': None,
+        'private_form': private_form
     }
     return context
