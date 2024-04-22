503ec8d5c51241d77d84b94f27432f3d643002e3
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-22 18:34:01 +0300

    Класс обработки отправленных сообщений изменен. Добавлена возможность работы с несколькими видами форм. Поведение метода пост изменяется в зависимости от того, какой тип чата получил сообщение.

--- django_paradox_mode_translation/chats/views.py
+++ django_paradox_mode_translation/chats/views.py
@@ -3,18 +3,25 @@ from channels.layers import get_channel_layer
 from django.http import JsonResponse
 from django.views.generic import FormView
 
-from chats.forms import SendMessageForm
-from chats.models import Chat
+from chats.forms import PrivateMessageForm, TeamMessageForm
+from chats.models import PrivateChat, TeamChat
 
 
 class SendMessageView(FormView):
-    form_class = SendMessageForm
+    private_form_class = PrivateMessageForm
+    group_form_class = TeamMessageForm
 
     def post(self, request, slug=None, *args, **kwargs):
         channel_layer = get_channel_layer()
-        form = self.get_form()
+        chat_type = request.POST.get('chat type')
+        form = self.get_form(chat_type=chat_type)
         if form.is_valid():
-            chat = Chat.objects.get(slug=slug)
+            if chat_type == "private":
+                chat = PrivateChat.objects.get(slug=slug)
+            elif chat_type == "team":
+                chat = TeamChat.objects.get(slug=slug)
+            else:
+                return
             instance = form.save(author=request.user, chat=chat)
             formatted_datetime = instance.pub_date.strftime("%Y-%m-%d %H:%M:%S")
             async_to_sync(channel_layer.group_send)(slug, {
@@ -24,3 +31,13 @@ class SendMessageView(FormView):
                 'pub_date': formatted_datetime
             })
         return JsonResponse({})
+
+    def get_form(self, form_class=None, chat_type=None):
+        if chat_type:
+            if chat_type == "private":
+                form_class = PrivateMessageForm
+            elif chat_type == "team":
+                form_class = TeamMessageForm
+            else:
+                return None
+        return super(SendMessageView, self).get_form(form_class)
