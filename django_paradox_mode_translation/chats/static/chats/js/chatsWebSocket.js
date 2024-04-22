720bb7a8c1d4ee1d896959a5313cd27f2fbeb895
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-22 15:08:27 +0300

    Написана функция для создания websocket соединения, а также происходит перехват отправки формы

---
+++ django_paradox_mode_translation/chats/static/chats/js/chatsWebSocket.js
@@ -0,0 +1,40 @@
+// chat_url = document.getElementById('chat').value
+
+function handshake(chat_url) {
+    let socket = new WebSocket('ws://' + window.location.host + '/' + 'ws/' + 'chats/' + chat_url + '/')
+    console.log(chat_url)
+
+    socket.onmessage = function (e) {
+
+        let data = JSON.parse(e.data);
+        let message = data.body;
+        let author = data.author;
+        let pub_date = data.pub_date
+        let chatMessages = $('#' + chat_url + '-chat-messages');
+        chatMessages.append((`<p><p>${pub_date}</p><b>${author}:</b> ${message}</p>`))
+    }
+
+    let form = $('#chat-form-' + chat_url)
+
+    form.submit(function (e) {
+        const csrfToken = Cookies.get('csrftoken')
+        e.preventDefault()
+        const full_url = 'http://' + window.location.host + '/' + 'chats/' + chat_url + '/'
+
+        $.ajax({
+            url: full_url,
+            method: "POST",
+            headers: {
+                'X-CSRFToken': csrfToken
+            },
+            data: $(e.target).serialize(),
+            success: function (response) {
+                console.log('response - ok')
+            },
+
+            error: function (error) {
+                console.log('Ошибка', error)
+            }
+        })
+    })
+}
