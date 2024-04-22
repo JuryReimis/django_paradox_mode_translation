c165ad0b1596cc5ff0e5bc5cd3583549d470baa0
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-22 20:16:59 +0300

    Зарегистрированы две новые модели

--- django_paradox_mode_translation/chats/admin.py
+++ django_paradox_mode_translation/chats/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from chats.models import PrivateChat, PrivateMessage
+from chats.models import PrivateChat, PrivateMessage, TeamChat, TeamMessage
 
 admin.site.register(PrivateChat)
 admin.site.register(PrivateMessage)
+admin.site.register(TeamChat)
+admin.site.register(TeamMessage)
