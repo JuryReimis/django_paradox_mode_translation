f33f38d5fddd8d09f8c39f71440db1a47d99f677
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-04-23 20:45:42 +0300

    Добавлена проверка на наличие искомого элемента

--- django_paradox_mode_translation/chats/static/chats/js/scrollFunctions.js
+++ django_paradox_mode_translation/chats/static/chats/js/scrollFunctions.js
@@ -1,9 +1,11 @@
 
 
 function scrollToPosition(elementId, chatUrl) {
-    const element = $('#' + elementId)
-
-    const scrollTo = element.position().top + (element.parent().height / 2)
-    console.log(scrollTo)
-    $('#collapse-' + chatUrl).scrollTop(scrollTo)
+    const element = $('#' + chatUrl + '-' + elementId)
+    if (element.length) {
+        const scrollTo = element.position().top - (element.parent().height() / 2)
+        console.log('element at', element.position().top)
+        console.log('scroll to', scrollTo)
+        $('#collapse-' + chatUrl).scrollTop(scrollTo)
+    }
 }
